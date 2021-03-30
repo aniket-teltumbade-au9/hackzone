4e29cec7f864b2a362259e377cb97590c3f02783
Shiv Mavala <shivmavala1006@gmail.com>
2021-03-30 14:00:15 +0530

    v1 Latest-stable

--- client/src/components/Admin/Contest/TableChallenge.jsx
+++ client/src/components/Admin/Contest/TableChallenge.jsx
@@ -4,23 +4,26 @@ import moment from "moment";
 export default function TableChallenge(props) {
   console.log("table", props)
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Difficulty</th>
-          <th>Max Score</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.tabledata.map(el => <tr>
-          <td>{el.name}</td>
-          <td>{el.level}</td>
-          <td>{el.points}</td>
-          <td>{moment(el.createdAt).fromNow()}</td>
-        </tr>)
+    <div className="w-100 bg-light" style={{ marginLeft: "-15px" }}>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Difficulty</th>
+            <th>Max Score</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.tabledata.map(el => <tr>
+            <td>{el.name}</td>
+            <td>{el.level}</td>
+            <td>{el.points}</td>
+            <td>{moment(el.createdAt).fromNow()}</td>
+          </tr>)
 
-        }</tbody>
-    </table>
+          }</tbody>
+      </table>
+    </div>
   )
 }
